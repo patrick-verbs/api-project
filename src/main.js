d053be5b302b5ff4051c0ae8a36e4580be9ba2fd
Patrick Lee <patricklee1138@live.com>
2021-04-23 12:15:08 -0700

    :construction: :poop: WIP: async logic structuring

--- src/main.js
+++ src/main.js
@@ -5,41 +5,68 @@ import './css/styles.css';
 import ExchangeService from './js/exchange-rate-service.js';
 
 function showExchangeRate(response) {
-  // if (response) {
-  //   if (response.PROPERTY === "VALUE") {
-  //     const someCurrencyProbably = response.PROPERTY;
-  // $("#element").text(response.PROPERTY);
-  // } else if (response.PROPERTY === "OTHER VALUE") {
-  //   const someOtherCurrencyMaybe = response.PROPERTY;
-  // $("#element").text(response.PROPERTY);
-  //   }
-  // }
+  if (response) {
+    if (response.PROPERTY === "VALUE") {
+      const someCurrencyProbably = response.PROPERTY;
+      $("#element").text(response.PROPERTY);
+    } else if (response.PROPERTY === "OTHER VALUE") {
+      const someOtherCurrencyMaybe = response.PROPERTY;
+      $("#element").text(response.PROPERTY);
+    }
+  }
 }
 
 function currencyNotFound() {
   // $("#currency-not-found").show();
 }
 
-async function parseExchangeRate(inputCurrency, outputCurrency) {
-  const response = await ExchangeService.getExchangeRate(inputCurrency, outputCurrency);
+async function fetchBaseCurrency(inputCurrency) {
+  const response = await ExchangeService.getExchangeRates(inputCurrency);
   if (response instanceof Error) {
-    console.log("Hey! This is an error!");
+    console.log(`Hey! This is an error!`);
     console.log(response);
     currencyNotFound();
   } else {
-    console.log(response.PROPERTY);
-    response.PROPERTY = outputCurrency;
+    // $("#currency-not-found").hide();
+    console.log(`Received exchange rates based on currency: ${response.base_code}`);
+    lastReceivedCurrency = [];
+    // search or "loop" through result.conversion_rates and locate outputCurrency
+    // response.PROPERTY = outputCurrency;
     showExchangeRate(response);
-    $("#could-not-find").hide();
+  }
+}
+
+function requestIsCached(requestedCurrency, cachedResponses) {
+  if (requestedCurrency === cachedResponses[0]) {
+    // Expand the check into a loop, and also compare current date/time to next update per API
+    // skip fetchBaseCurrency
+    return false;
+  } else {
+    return true;
   }
 }
 
 $(document).ready(function () {
+  // Refactor this to choose the input currency on the first menu
+  // ...then fetch, and create <select> options from the "result.conversion_rates" entries
+  // ...and therefore do NOT fetch when a user requests a conversion; just do the math
+  //
+  // For now, using scoped variables to compare the user's chosen currency (e.g. USD)
+  // to the last fetched one, to prevent spamming the fetches (until page refresh, anyway)
+  let cachedResponses = [];
+  let baseCurrency;
   $("#currency-submit").on("click", function() {
     const inputQuantity = $("#input-quantity").val();
-    const inputCurrency = $("#input-currency").val().toUpperCase();
+    const inputCurrency = $("#input-currency").val().toUpperCase();// All 3-letter currency values in ExchangeRate-API are uppercase
     const outputCurrency = $("#output-currency").val().toUpperCase();
     console.log(inputQuantity, inputCurrency, outputCurrency);
-    // parseExchangeRate(inputCurrency, outputCurrency);
+    if (!requestIsCached(inputCurrency, cachedResponses)) {
+      // if the base unit & time is not yet stored/fetched, fetch it
+      // and add to the array of cached responses
+      cachedResponses.push(fetchBaseCurrency(inputCurrency));
+    }
+    // Refactor this to search the array for the correct response (instead of just the most recent one)
+    baseCurrency = cachedResponses[cachedResponses.length - 1];
+    showExchangeRate(baseCurrency, outputCurrency);
   });
 });
