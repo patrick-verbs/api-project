2d6bb02eff98bc9f945c303883c6f80f41f64b75
Patrick Lee <patricklee1138@live.com>
2021-05-03 08:24:31 -0700

    :sparkles: add robust error handling and progress-log output to UI; refactoring

--- src/main.js
+++ src/main.js
@@ -5,8 +5,19 @@ import './css/styles.css';
 import CACHED_RESPONSES from './js/cache.js';
 import ExchangeService from './js/exchange-rate-service.js';
 
-function currencyNotFound() {
-  // $("#currency-not-found").show();
+function resetDisplay() {
+  $("#output-result").show();
+  $("#progress-log").html("");
+  const errorConsole = $("#error-console");
+  errorConsole.hide();
+  errorConsole.html("");
+}
+
+function printError(message) {
+  $("#output-result").hide();
+  const errorConsole = $("#error-console");
+  errorConsole.append(`<li>${message}</li>`);
+  errorConsole.show();
 }
 
 function buildInputOptions(currencyCode) {
@@ -18,7 +29,10 @@ function buildInputOptions(currencyCode) {
   for (let i = 0; i < arrayOfCurrencies.length; i++) {
     html += `<option value="${arrayOfCurrencies[i]}" require>${arrayOfCurrencies[i]}</option>`;
   }
-  return html;
+  return {
+    html_string: html,
+    currency_codes: arrayOfCurrencies
+  };
 }
 
 function showExchangeRate(userQuery) {
@@ -26,89 +40,115 @@ function showExchangeRate(userQuery) {
   const allCurrencies = userQuery.response.conversion_rates;
 
   let conversionRate;
-  // let html;
 
+  userQuery.progress_log.append(`<li><ul>Searching for output currency "${userQuery.output_currency}":`);
   for (const currency in allCurrencies) {
-    console.log(`${currency}: ${allCurrencies[currency]}`);
-    // html += currency;
+    userQuery.progress_log.append(`<li>${currency}: ${allCurrencies[currency]}</li>`);
     if (currency === userQuery.output_currency) {
       conversionRate = allCurrencies[currency];
       break;
     }
   }
+  userQuery.progress_log.append(`</ul></li>`);
 
   const convertedCurrency = (userQuery.input_quantity * conversionRate).toFixed(2);
 
   if (response.result === "success") {
-    // const someCurrencyProbably = response.PROPERTY;
-    $("#output-result").html(`<h3>Here's the API query result: ${convertedCurrency}</h3>`);
-  // } else if (response.PROPERTY === "OTHER VALUE") {
-    // const someOtherCurrencyMaybe = response.PROPERTY;
-    // $("#output-result").html(`${response.PROPERTY}`);
+    $("#output-result").html(`<h3>${userQuery.input_quantity} ${userQuery.input_currency} converts to: <br> <strong><em>${convertedCurrency} ${userQuery.output_currency}</em></strong></h3>`);
+  } else {
+    printError(`An error occurred: <em>${response["error-type"]}</em>. Please verify the URL, including your private API key.`);
   }
 }
 
 async function parseBaseCurrency(userQuery) {
   let response;
   if (userQuery.do_fetch === true) {
-    console.log(`Query not yet cached: fetching ${userQuery.input_currency} data via API`);
+    userQuery.progress_log.append(`<li>Query not yet cached: fetching ${userQuery.input_currency} data via API</li>`);
     response = await ExchangeService.getExchangeRates(userQuery.input_currency);
     CACHED_RESPONSES.unshift(response);
   } else {
-    console.log(`Query is cached: bypassing API fetch`);
+    userQuery.progress_log.append(`<li>Query is cached: bypassing API fetch</li>`);
     response = CACHED_RESPONSES[0];
   }
   if (response instanceof Error) {
-    console.log(`Hey! This is an error!`);
-    console.log(response);
-    currencyNotFound();
+    const errorMessage = `There was an error: ${response.message}`;
+    printError(errorMessage);
   } else {
-    // $("#currency-not-found").hide();
-    console.log(`Received exchange rates based on currency: ${response.base_code}`);
-    // // // response.PROPERTY = outputCurrency;
+    $("#error-console").hide();
+    userQuery.progress_log.append(`<li>Received exchange rates based on currency: ${response.base_code}</li>`);
     userQuery.response = response;
     showExchangeRate(userQuery);
   }
 }
 
 function requestNotCached(currencyCode) {
-  if (CACHED_RESPONSES[0] != undefined && currencyCode === CACHED_RESPONSES[0].base_code) {
-    // Expand the check into a loop, and also compare current date/time to next update per API
-    // skip parseBaseCurrency
-    return false;
-  } else {
-    return true;
+  const currentTime = Date.now() / 1000;
+  for (let i = 0; i < CACHED_RESPONSES.length; i++) {
+    if (currencyCode === CACHED_RESPONSES[i].base_code
+    && currentTime >= CACHED_RESPONSES[i].time_last_update_unix
+    && currentTime < CACHED_RESPONSES[i].time_next_update_unix
+    ) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
 
 $(document).ready(function () {
   let latestCache = CACHED_RESPONSES[0];
-  const html = buildInputOptions(latestCache.conversion_rates);
-  $("#output-currency").html(html);
-  // Refactor this to choose the input currency on the first menu
-  // ...then fetch, and create <select> options from the "result.conversion_rates" entries
-  // ...and therefore do NOT fetch when a user requests a conversion; just do the math
-  //
-  // For now, using scoped variables to compare the user's chosen currency (e.g. USD)
-  // to the last fetched one, to prevent spamming the fetches (until page refresh, anyway)
-  // let inputCurrencyResponse;
+  const allCurrencyCodes = buildInputOptions(latestCache.conversion_rates);
+  $("#input-currency").html(allCurrencyCodes.html_string);
+  $("#output-currency").html(allCurrencyCodes.html_string);
+
   $("#currency-submit").on("click", function() {
-    const inputQuantity = $("#input-quantity").val();
-    const inputCurrency = $("#input-currency").val().toUpperCase();// All 3-letter currency values in ExchangeRate-API are uppercase
+    resetDisplay();
+
+    // All 3-letter currency codes in ExchangeRate-API are uppercase
+    const inputCurrency = $("#input-currency").val().toUpperCase();
     const outputCurrency = $("#output-currency").val().toUpperCase();
+    
+    let inputErrors = false;
+    
+    const validInputCurrency = Object.prototype.hasOwnProperty.call(latestCache["conversion_rates"], inputCurrency);
+    if (!validInputCurrency) {
+      inputErrors = true;
+      printError(`"${inputCurrency}" is not a recognized currency code so its quantity cannot be converted.`);
+    }
+    
+    const validOutputCurrency = Object.prototype.hasOwnProperty.call(latestCache["conversion_rates"], outputCurrency);
+    if (!validOutputCurrency) {
+      inputErrors = true;
+      printError(`"${outputCurrency}" is not a recognized currency code so no value can be converted into it.`);
+    }
+    
+    const inputQuantity = parseFloat($("#input-quantity").val());
+    const inputQuantityShortened = inputQuantity.toFixed(2);
+    if (isNaN(inputQuantity)
+    || inputQuantity < 0
+    || inputQuantity > 10000000000000
+    || inputQuantityShortened != inputQuantity) {
+      inputErrors = true;
+      printError(`"${inputQuantity}" is not a valid number between 0 and 10 trillion, or exceeds the two-decimal-place limit.`);
+    }
 
-    // returns 'true' if the base unit is not yet fetched/cached
-    const doFetch = requestNotCached(inputCurrency);
+    if (!inputErrors) {
+      const doFetch = requestNotCached(inputCurrency);
+      // returns 'true' if the base unit's response is not yet fetched/cached for the current date
 
-    const userQuery = {
-      do_fetch: doFetch,
-      input_quantity: inputQuantity,
-      input_currency: inputCurrency,
-      output_currency: outputCurrency
-    };
+      const progressLog = $("#progress-log");
+      progressLog.html("");
+      // used to show console-like progress to the user
 
-    parseBaseCurrency(userQuery);
-    // Refactor this to search the array for the correct response (instead of just the most recent one)
-    // inputCurrencyResponse = CACHED_RESPONSES[CACHED_RESPONSES.length - 1];
+      const userQuery = {
+        do_fetch: doFetch,
+        input_quantity: inputQuantity,
+        input_currency: inputCurrency,
+        output_currency: outputCurrency,
+        progress_log: progressLog
+      };
+
+      parseBaseCurrency(userQuery);
+    }
   });
 });
